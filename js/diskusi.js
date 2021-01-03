7633234458541c82b92b5eff17fd0a5fff6edaf4
rifky290301 <rifky290301@gmail.com>
2021-01-03 20:16:37 +0700

    finishing 1

--- js/diskusi.js
+++ js/diskusi.js
@@ -6,3 +6,15 @@ $(document).ready(function () {
   });
 
 });
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() >= 500) {
+//     $('#tombol').css('position', 'fixed');
+//     $('#tombol').css('left', '2rem');
+//     $('#tombol').css('bottom', '5rem');
+//     // $('#teks-bar').css('color', 'black');
+//   } else {
+//     $('#tombol').css('position', 'unset');
+//     // $('#teks-bar').css('color', 'white');
+//   }
+// });
