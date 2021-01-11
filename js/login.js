f5687baf8a0b5ef8ab2e2466ad7b171f8927fff0
TakayumAja <asraftakayuma76@gmail.com>
2021-01-12 00:13:58 +0700

    js validasi email address

--- js/login.js
+++ js/login.js
@@ -9,3 +9,25 @@ function notif2() {
 
     alert("Trimaksih "+nama+" Sudah Melakukan Register")
 }
+
+function validasiemail() {
+    var form =document.getElementById("form");
+    var email=document.getElementById("email").value;
+    var text =document.getElementById("text");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Your Email Address is valid.";
+        text.style.color= "blue";
+        text.style.fontSize ="14px";
+    }
+    else{
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "Please Enter Valid Email Address.";
+        text.style.color= "red";
+        text.style.fontSize ="14px";
+    }
+}
